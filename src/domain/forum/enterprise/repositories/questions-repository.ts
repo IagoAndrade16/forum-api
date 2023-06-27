b3d9b8675148c58ad4da3d524844f345fb113e85
Iago Alexandre <iagoaap@outlook.com>
2023-06-27 19:55:35 -0300

    list recent questions use case

--- src/domain/forum/enterprise/repositories/questions-repository.ts
+++ src/domain/forum/enterprise/repositories/questions-repository.ts
@@ -1,9 +1,11 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../entities/question'
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>
   create(question: Question): Promise<void>
   findBySlug(slug: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[] | null>
   delete(question: Question): Promise<void>
   save(question: Question): Promise<void>
 }
